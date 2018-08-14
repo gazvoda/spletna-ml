a582466b008454bef459403ced215df0be46d815
ep <ep@ep>
2018-08-14 17:34:08 +0200

    Init

---
+++ db/database_spletna.php
@@ -0,0 +1,124 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require_once 'database_init.php';
+
+class DBSpletna {
+
+    public static function getAllUsers() {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("SELECT id, vloga, ime, priimek, email FROM uporabnik");
+        $statement->execute();
+
+        return $statement->fetchAll();
+    }
+/*
+    public static function getAllRole($user_role) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("SELECT user_id, role, first_name, last_name, email FROM user WHERE user_role = :user_role");
+        $statement->bindParam(":user_role", $user_role);
+        $statement->execute();
+
+        return $statement->fetchAll();
+    }
+
+    public static function delete($user_id) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("DELETE FROM user WHERE user_id = :user_id");
+        $statement->bindParam(":user_id", $user_id, PDO::PARAM_INT);
+        $statement->execute();
+    }
+
+    public static function get($user_id) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("SELECT user_id, role, first_name, last_name, email, password, status FROM user 
+            WHERE user_id =:user_id");
+        $statement->bindParam(":user_id", $user_id, PDO::PARAM_INT);
+        $statement->execute();
+
+        return $statement->fetch();
+    }
+
+    public static function insert($role, $first_name, $last_name, $email, $password, $phone, $address, $status) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("INSERT INTO user (role, first_name, last_name, email, password, phone, address, status)
+            VALUES (:role, :first_name, :last_name, :email, :password, :phone, :address, :status)");
+        $statement->bindParam(":role", $role);
+        $statement->bindParam(":first_name", $first_name);
+        $statement->bindParam(":last_name", $last_name);
+        $statement->bindParam(":email", $email);
+        $statement->bindParam(":password", $password);
+        $statement->bindParam(":phone", $phone);
+        $statement->bindParam(":address", $address);
+        $statement->bindParam(":status", $status);
+        
+        $statement->execute();
+    }
+
+    public static function updatePassword($user_id, $password) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("UPDATE user SET password = :password WHERE user_id =:user_id");
+        $statement->bindParam(":password", $password);
+        $statement->bindParam(":user_id", $user_id, PDO::PARAM_INT);
+        $statement->execute();
+    }
+    
+    public static function updateFirstName($user_id, $first_name) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("UPDATE user SET first_name = :first_name WHERE user_id =:user_id");
+        $statement->bindParam(":first_name", $first_name);
+        $statement->bindParam(":user_id", $user_id, PDO::PARAM_INT);
+        $statement->execute();
+    }
+    
+    public static function updateLastName($user_id, $last_name) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("UPDATE user SET last_name = :last_name WHERE user_id =:user_id");
+        $statement->bindParam(":last_name", $last_name);
+        $statement->bindParam(":user_id", $user_id, PDO::PARAM_INT);
+        $statement->execute();
+    }
+    
+    public static function updateEmail($user_id, $email) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("UPDATE user SET email = :email WHERE user_id =:user_id");
+        $statement->bindParam(":email", $email);
+        $statement->bindParam(":user_id", $user_id, PDO::PARAM_INT);
+        $statement->execute();
+    }
+    
+    public static function updatePhone($user_id, $phone) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("UPDATE user SET phone = :phone WHERE user_id =:user_id");
+        $statement->bindParam(":phone", $phone);
+        $statement->bindParam(":user_id", $user_id, PDO::PARAM_INT);
+        $statement->execute();
+    }
+    
+    public static function updateAddress($user_id, $address) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("UPDATE user SET address = :first_name WHERE user_id =:user_id");
+        $statement->bindParam(":address", $address);
+        $statement->bindParam(":user_id", $user_id, PDO::PARAM_INT);
+        $statement->execute();
+    }
+*/
+    
+    
+}
---
+++ db/spletna_trgovina.sql
@@ -0,0 +1,110 @@
+-- MySQL dump 10.13  Distrib 5.7.22, for Linux (x86_64)
+--
+-- Host: localhost    Database: spletna_trgovina
+-- ------------------------------------------------------
+-- Server version	5.7.22-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `artikel`
+--
+
+DROP TABLE IF EXISTS `artikel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `artikel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `opis` text,
+  `cena` float NOT NULL,
+  `zaloga` int(11) DEFAULT NULL,
+  `ime` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `artikel`
+--
+
+LOCK TABLES `artikel` WRITE;
+/*!40000 ALTER TABLE `artikel` DISABLE KEYS */;
+INSERT INTO `artikel` VALUES (1,'Copati iz bombaza, rjavi',12,100,'Copati'),(2,'Usnjeni copati, rjavi',16,50,'Copati, usnjeni');
+/*!40000 ALTER TABLE `artikel` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `racun`
+--
+
+DROP TABLE IF EXISTS `racun`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `racun` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `postavka` float NOT NULL,
+  `status` text NOT NULL,
+  `item_ids` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `racun`
+--
+
+LOCK TABLES `racun` WRITE;
+/*!40000 ALTER TABLE `racun` DISABLE KEYS */;
+/*!40000 ALTER TABLE `racun` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `uporabnik`
+--
+
+DROP TABLE IF EXISTS `uporabnik`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `uporabnik` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `vloga` text NOT NULL,
+  `ime` text NOT NULL,
+  `priimek` text NOT NULL,
+  `email` text NOT NULL,
+  `geslo` text NOT NULL,
+  `telefon` int(11) DEFAULT NULL,
+  `naslov` text,
+  `status` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `uporabnik`
+--
+
+LOCK TABLES `uporabnik` WRITE;
+/*!40000 ALTER TABLE `uporabnik` DISABLE KEYS */;
+INSERT INTO `uporabnik` VALUES (1,'administrator','lenart','gazvoda','gazvoda@localhost.com','admin',NULL,NULL,'aktiven'),(2,'administrator','mihael','podplatnik','podplatnik@localhost.com','admin',NULL,NULL,'aktiven'),(3,'prodajalec','joze','novak','novak@localhost.com','prodajalec',NULL,NULL,'aktiven'),(4,'prodajalec','tanja','zupancic','zupancic@localhost.com','prodajalec',NULL,NULL,'neaktiven'),(5,'stranka','miha','pecnik','pecnik@localhost.com','stranka',51051051,'Naslov 1, 1000 Ljubljana, Slovenija','aktiven'),(6,'stranka','jan','hartman','hartman@localhost.com','stranka',31031031,'Naslov 2, 8000 Novo mesto, Slovenija','neaktiven');
+/*!40000 ALTER TABLE `uporabnik` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-08-14 17:32:30
