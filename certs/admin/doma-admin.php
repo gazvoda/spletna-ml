9d11022f33ac9e38a817dcd684711d5d0e2e5a50
ep <ep@ep>
2018-08-16 21:52:43 +0200

    Change user name of seller

--- certs/admin/doma-admin.php
+++ certs/admin/doma-admin.php
@@ -29,56 +29,80 @@ require_once '../../db/database_spletna.php';
         </form>
     
         <?php
-        // VNOS -- ZASLONSKA MASKA
+        // VNOS PRODAJALCA -- ZASLONSKA MASKA
         if (isset($_GET["do"]) && $_GET["do"] == "add"):
             ?>
-            <h1>Dodajanje</h1>
+            <h1>Dodaj prodajalca</h1>
             <form action="<?= $_SERVER["PHP_SELF"] ?>" method="post">
                 <input type="hidden" name="do" value="add" />
-                Datum: <input type="text" name="joke_date" value="<?= date("Y-m-d") ?>" /><br />
-                <textarea rows="8" cols="60" name="joke_text"></textarea><br />
-                <input type="submit" value="Shrani" />
+                <input type="text" name="ime_prodajalca" placeholder="Ime" /><br />
+                <input type="text" name="priimek_prodajalca" placeholder="Priimek" /><br />
+                <input type="text" name="email_prodajalca" placeholder="Email" /><br />
+                <input type="password" name="geslo_prodajalca" placeholder="Geslo" /><br />
+                <input type="submit" value="Dodaj" />
             </form>
             <?php
-        // UREJANJE -- ZASLONSKA MASKA
+        // UREJANJE PRODAJALCA -- ZASLONSKA MASKA
         elseif (isset($_GET["do"]) && $_GET["do"] == "edit"):
             ?>
             <h1>Urejanje</h1>
             <?php
             try {
-                $joke = DBJokes::get($_GET["id"]); // POIZVEDBA V PB
+                $prodajalec = DBSpletna::getUser($_GET["id"])[0]; // POIZVEDBA V PB
+                //var_dump($prodajalec);
             } catch (Exception $e) {
                 echo "Napaka pri poizvedbi: " . $e->getMessage();
             }
 
-            $id = $joke["id"];
-            $date = $joke["joke_date"];
-            $text = $joke["joke_text"];
+            $id = $prodajalec["id"];
+            $ime = $prodajalec["ime"];
+            $priimek = $prodajalec["priimek"];
+            $email = $prodajalec["email"];
             ?>
             <h2>Urejanje zapisa id = <?= $id ?></h2>
             <form action="<?= $_SERVER["PHP_SELF"] ?>" method="post">
                 <input type="hidden" name="id" value="<?= $id ?>" />
-                <input type="hidden" name="do" value="edit" />
-                Datum: <input type="text" name="joke_date" value="<?= $date ?>" /><br />
-                <textarea rows="8" cols="60" name="joke_text"><?= $text ?></textarea><br />
-                <input type="submit" value="Shrani" />
+                <input type="hidden" name="do" value="edit_ime" />
+                <input type="text" name="ime_prodajalca" placeholder="Ime" />
+                <input type="submit" value="Spremeni" />
+            </form>
+            <form action="<?= $_SERVER["PHP_SELF"] ?>" method="post">
+                <input type="hidden" name="id" value="<?= $id ?>" />
+                <input type="hidden" name="do" value="edit_priimek" />
+                <input type="text" name="priimek_prodajalca" placeholder="Priimek" />
+                <input type="submit" value="Spremeni" />
+            </form>
+            <form action="<?= $_SERVER["PHP_SELF"] ?>" method="post">
+                <input type="hidden" name="id" value="<?= $id ?>" />
+                <input type="hidden" name="do" value="edit_email" />
+                <input type="text" name="email_prodajalca" placeholder="Email" />
+                <input type="submit" value="Spremeni" />
+            </form>
+            <form action="<?= $_SERVER["PHP_SELF"] ?>" method="post">
+                <input type="hidden" name="id" value="<?= $id ?>" />
+                <input type="hidden" name="do" value="edit_geslo" />
+                <input type="password" name="geslo_prodajalca" placeholder="Geslo" />
+                <input type="submit" value="Spremeni" />
             </form>
 
-            <h2>Izbris zapisa</h2>
+            <h2>Izbris prodajalca</h2>
             <form action="<?= $_SERVER["PHP_SELF"] ?>" method="post">
                 <input type="hidden" name="id" value="<?= $id ?>" />
                 <input type="hidden" name="do" value="delete" />
                 <input type="submit" value="Briši" />
             </form>		
             <?php
-        // POSODABLJANJE ZAPISA V PB
-        elseif (isset($_POST["do"]) && $_POST["do"] == "edit"):
+        // POSODABLJANJE ZAPISA IMENA PRODAJALCA V PB
+        elseif (isset($_POST["do"]) && $_POST["do"] == "edit_ime"):
             ?>
             <h1>Posodobitev zapisa</h1>
             <?php
             try {
-                DBJokes::update($_POST["id"], $_POST["joke_date"], $_POST["joke_text"]);
-                echo "Šala uspešno posodobljena. <a href='$_SERVER[PHP_SELF]'>Na prvo stran.</a></p>";
+                // var_dump($_POST);
+                // var_dump($_SESSION["ime_prodajalca"]);
+                // die();
+                DBSpletna::updateFirstName($_POST["id"], $_POST["ime_prodajalca"]);
+                echo "Ime uspešno posodobljeno. <a href='$_SERVER[PHP_SELF]'>Na prvo stran.</a></p>";
             } catch (Exception $e) {
                 echo "<p>Napaka pri zapisu: {$e->getMessage()}.</p>";
             }
@@ -115,6 +139,8 @@ require_once '../../db/database_spletna.php';
             try {
                 $vsi_prodajalci = DBSpletna::getAllRole("prodajalec");
                 var_dump($vsi_prodajalci);
+                $orders = DBSpletna::getAllOrders();
+                var_dump($orders);
             } catch (Exception $e) {
                 echo "Prišlo je do napake: {$e->getMessage()}";
             }
